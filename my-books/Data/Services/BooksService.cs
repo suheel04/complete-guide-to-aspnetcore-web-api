11d730d7c763df88628a210ac07db9b0b4c598e9
Suheel <54589639+suheel04@users.noreply.github.com>
2021-07-28 21:11:24 +0530

    Commit 24

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -48,8 +48,26 @@ namespace my_books.Data.Services
 
         public List<Book> GetAllBooks() => _context.Books.ToList();
 
-        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n=>n.Id ==bookId);
+        //public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n=>n.Id ==bookId);
+        public BookWithAuthorsVM GetBookById(int bookId)
+        {
+            var _bookWithAuthors = _context.Books.Where(n=> n.Id == bookId).Select(book => new BookWithAuthorsVM()
+            {
 
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                CoverUrl = book.CoverUrl,
+                PublisherName = book.Publisher.Name,
+                AuthorNames = book.Book_Authors.Select(n => n.Author.FullName).ToList()
+
+            }).FirstOrDefault();
+
+            return _bookWithAuthors;
+        }
         public Book UpdateBookById(int bookId, BookVM book)
         {
             var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
