979439899f8e7e2725ef049fa9059efe6a4fe64b
Suheel <54589639+suheel04@users.noreply.github.com>
2021-08-09 20:48:00 +0530

    Section 38

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -20,11 +20,11 @@ namespace my_books.Controllers
             _publishersService = publishersService;
         }
         [HttpGet("get-all-publishers")]
-        public IActionResult GetAllPublishers(string sortBy, string searchString)
+        public IActionResult GetAllPublishers(string sortBy, string searchString, int pageNumber)
         {
             try 
             {
-                var _result = _publishersService.GetAllPublishers(sortBy, searchString);
+                var _result = _publishersService.GetAllPublishers(sortBy, searchString, pageNumber);
                 return Ok(_result);
             }
             catch 
--- my-books/Data/Services/PublishersService.cs
+++ my-books/Data/Services/PublishersService.cs
@@ -1,4 +1,5 @@
 ﻿using my_books.Data.Models;
+using my_books.Data.Paging;
 using my_books.Data.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,7 @@ namespace my_books.Data.Services
             _context = context;
         }
 
-        public List<Publisher> GetAllPublishers(string sortBy, string searchString)
+        public List<Publisher> GetAllPublishers(string sortBy, string searchString, int? pageNumber)
         {
             var allPublishers = _context.Publishers.OrderBy(n => n.Name).ToList();
 
@@ -35,6 +36,11 @@ namespace my_books.Data.Services
             {
                 allPublishers = allPublishers.Where(n => n.Name.Contains(searchString,StringComparison.CurrentCultureIgnoreCase)).ToList();
             }
+
+            //Paging
+            int pageSize = 5;
+            allPublishers = PaginatedList<Publisher>.Create(allPublishers.AsQueryable(), pageNumber ?? 1, pageSize);
+
             return allPublishers;
         }
         public void AddPublisher(PublisherVM publisher)
