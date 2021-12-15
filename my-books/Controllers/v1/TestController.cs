6c4b9f9891766567639dfb676a4bece97c9109f6
Suheel <suheel04@gmail.com>
2021-12-15 21:26:55 +0530

    Section6 end API Versioning

---
+++ my-books/Controllers/v1/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers.v1
+{
+    
+
+    [ApiVersion("1.0")]
+    [ApiVersion("1.2")]
+    [ApiVersion("1.9")]
+    [Route("api/[controller]")]
+    //[Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("get-test-data")]
+        public IActionResult Get()
+        {
+            return Ok("This is the Version V1");
+        }
+
+        [HttpGet("get-test-data"), MapToApiVersion("1.2")]
+        public IActionResult GetV12()
+        {
+            return Ok("This is the Version V1.2");
+        }
+
+        [HttpGet("get-test-data"), MapToApiVersion("1.9")]
+        public IActionResult Getv19()
+        {
+            return Ok("This is the Version V1.9");
+        }
+    }
+}
--- my-books/Startup.cs
+++ my-books/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +40,14 @@ namespace my_books
             services.AddTransient<BooksService>();
             services.AddTransient<AuthorsService>();
             services.AddTransient<PublishersService>();
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+
+                //config.ApiVersionReader = new HeaderApiVersionReader("custom-version-header");
+                //config.ApiVersionReader = new MediaTypeApiVersionReader();
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v2", new OpenApiInfo { Title = "my_books_updated_title", Version = "v2" });
